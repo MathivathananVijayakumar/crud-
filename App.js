4c65cac298c1c3c6621fdfc745c712d3fd744892
Mathivathanan <43021541+MathivathananVijayakumar@users.noreply.github.com>
2021-08-26 10:10:41 +0530

    commited

--- App.js
+++ App.js
@@ -2,24 +2,12 @@
 import {BrowserRouter,Route,NavLink} from 'react-router-dom'
 import Post from './Post'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './Home'
-import About from './About';
+
 
 function App() {
   return (<>
- 
-    <BrowserRouter>
-    {/* <NavLink to='/'>Home</NavLink><br/>
-    <NavLink to='/About'>About</NavLink><br/> */}
-    <NavLink to='/Post'>Post</NavLink>
-     <Route path exact= "/">
-    <Home/>
-    </Route> 
-  
-    <Route path ="/About"><About/>
-    </Route>
-
-  
+  <BrowserRouter>
+  <NavLink to='/Post'>Post</NavLink>
   <Route path ="/Post"><Post/>
     </Route>
   </BrowserRouter>
